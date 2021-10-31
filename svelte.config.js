a17508a6a858e2f072fc19075a6f57bd4252747e
Lauren Chua <laurenchuaweiqian2008@gmail.com>
2021-10-31 06:19:09 +0000

    actual commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,28 @@
-import vercel from '@sveltejs/adapter-vercel';
-
+import preprocess from 'svelte-preprocess';
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-   	 // hydrate the <div id="svelte"> element in src/app.html
-   	 target: '#svelte',
-   	 adapter: vercel()
-    }
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+
+		vite: {
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: '@import "src/variables.scss";'
+					}
+				}
+			}
+		}
+	},
+
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: '@import "src/variables.scss";'
+			}
+		})
+	]
 };
 
 export default config;
