65048666f88292feabaf93fc0d129d3ea09d567f
Lauren Chua <laurenchuaweiqian2008@gmail.com>
2021-11-07 07:18:35 +0000

    commiting yet again

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let gitHub
+</script>
+
+<a>
+    {gitHub}
+</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from "$lib/Header.svelte";
+    import Section from "$lib/Section.svelte";
+    import Footer from "$lib/Footer.svelte";
+    let myInfo = {
+    title: "Info",
+    item1: "Lauren Chua Wei Qian",
+    item2: 13,
+    item3: "Chung Ling Private High School",
+}
+    let myHobbies = {
+    title: "Hobbies",
+    item1: "Surfing the Internet",
+    item2: "Drawing",
+    item3: "Daydreaming",
+}
+    let myPersonality = {
+    title: "Personality",
+    item1: "Stubborn",
+    item2: "Eccentric",
+    item3: "Rebellious",
+}
+</script>
+
+<Header title={"My Biography"}/>
+<Section {...myInfo}{...myHobbies}{...myPersonality}/>
+<Footer gitHub={"https://github.com/InsanityZones/lauren-my-biography"}/>
